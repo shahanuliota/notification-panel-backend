676b67ba70888e5f3d7643e65888ac95c751dbd1
shahanuliota <shahanul.iotait@gmail.com>
2022-10-31 17:07:04 +0600

    logger

--- src/common/logger/controller/logger.controller.ts
+++ src/common/logger/controller/logger.controller.ts
@@ -1,5 +1,5 @@
 import {Controller, Get, Res, StreamableFile} from '@nestjs/common';
-import {createReadStream} from 'fs';
+import {createReadStream, readdirSync, statSync} from 'fs';
 import {join} from 'path';
 import type {Response} from 'express';
 
@@ -17,6 +17,36 @@ export class LoggerController {
             'Content-Disposition': 'attachment; filename="2022-10-24.log"',
         });
         return new StreamableFile(file);
+    }
+
+
+    @Get('/files')
+    fileName() {
+
+
+        const getAllFiles = (dirPath, arrayOfFiles) => {
+            const files = readdirSync(dirPath);
+
+            arrayOfFiles = arrayOfFiles || {};
+            const fileList = [];
+
+            files.forEach(function (file) {
+                if (statSync(dirPath + "/" + file).isDirectory()) {
+                    arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles);
+                } else {
+                    if (file.endsWith('.log')) {
+                        fileList.push(dirPath + "/" + file);
+                    }
+                }
+            });
+            if (fileList.length !== 0) {
+                arrayOfFiles[dirPath] = fileList;
+            }
+
+            return arrayOfFiles;
+        };
+        const result = getAllFiles("logs", {});
+        return {"ok": result};
     }
 
 }
